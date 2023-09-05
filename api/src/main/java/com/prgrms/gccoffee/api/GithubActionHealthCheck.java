90028f93a1d1cb120fb5cee3e8b95f8ec8dd21a7
joyfulviper <kimsk3113@naver.com>
2023-09-05 22:49:03 +0900

    test: github action 테스트

---
+++ api/src/main/java/com/prgrms/gccoffee/api/GithubActionHealthCheck.java
@@ -0,0 +1,13 @@
+package com.prgrms.gccoffee.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GithubActionHealthCheck {
+
+    @GetMapping("/api/v1/health")
+    public String check() {
+        return "헬스체크";
+    }
+}
